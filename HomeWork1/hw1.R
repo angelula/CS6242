9a9bc4fe90c747984554f384c65122ba8b812b84
angelula <angelucci.louis@gmail.com>
2018-05-28 01:16:19 +0200

    Homework items 2-4 implemented.y

--- HomeWork1/hw1.R
+++ HomeWork1/hw1.R
@@ -1,4 +1,7 @@
+install.packages("ggplot2")
 setwd("~/CS6242/HomeWork1/")
+
+options(expressions=500000)
 
 log_gamma_loop <- function(n) {
 	if(n<=1)
@@ -32,3 +35,23 @@ sum_log_gamma_recursive <- function(n) {
   return(sum)
 }
 
+sum_lgamma <- function(n) {
+  sum<-0
+  for(i in seq(1,n,1)) {
+    sum<-sum+lgamma(i)
+  }
+  return(sum)
+}
+
+n = seq(1, 100, by=1)
+
+ssum_log_gamma_loop = sapply(n, function(v) {
+  start = Sys.time()
+  sum_log_gamma_loop(v)
+  end = Sys.time()
+  return (end-start)
+})
+
+png ("sum_log_gamma_loop.png",480,480,"px",12)
+plot(n,ssum_log_gamma_loop,main="Gamma Loop", ylab="Run Time", xlab="N")
+dev.off()
